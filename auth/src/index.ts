a1796e66d321dbee206020eef50a0b774e447cbe
JFernando12 <fire.jf2012@gmail.com>
2022-07-27 08:28:48 -0500

    changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,10 @@ app.use(express.json());
 app.use(express.urlencoded( { extended: false }));
 app.use(morgan('dev'));
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there!');
+});
+
 app.listen(3000, () => {
-    console.log('Server on port 3000');
+    console.log('Server on port 3000!!!!!');
 })
