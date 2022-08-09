c8429e6ac2213fc481f33992d602c713ad5ca19f
JFernando12 <fire.jf2012@gmail.com>
2022-08-09 10:24:13 -0500

    add mongo with typescript

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,22 +7,29 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
+import mongoose from "mongoose";
+
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: false }));
 app.use(morgan('dev'));
 
-app.use('/api', currentuserRouter);
-app.use('/api', signinRouter);
-app.use('/api', signoutRouter);
-app.use('/api', signupRouter);
+app.use('/api/users', currentuserRouter);
+app.use('/api/users', signinRouter);
+app.use('/api/users', signoutRouter);
+app.use('/api/users', signupRouter);
 
 app.all('*', async() => {
     throw new NotFoundError();
 })
 
 app.use(errorHandler);
+
+mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    .then(db => console.log('DB connected'))
+    .catch(err => console.error(err));
+
 
 app.listen(3000, () => {
     console.log('Server on port 3000');
