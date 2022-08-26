33cd7cacdcb1676c66f5123bdb82bca5d3961728
JFernando12 <fire.jf2012@gmail.com>
2022-08-26 07:29:12 -0500

    client added

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import "express-async-errors";
-import morgan from 'morgan';
 import cookieSession from "cookie-session";
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
@@ -15,7 +14,6 @@ app.set('trust proxy', true);
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: false }));
-app.use(morgan('dev'));
 app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
