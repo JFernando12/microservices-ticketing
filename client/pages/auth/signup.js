72d4e22126263e76aacbf3f3b6887d999be1727e
JFernando12 <fire.jf2012@gmail.com>
2022-08-28 07:58:20 -0500

    axios hook added

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,25 +1,21 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router'
+import useRequest from '../../hooks/use-request';
 
 export default () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: { email, password},
+        onSuccess: () => Router.push('/')
+    })
 
     const onSubmit = async (event) => {
-        try {
-            event.preventDefault();
-            const response = await axios.post("/api/users/signup", {
-                email,
-                password
-            })
-            console.log(response);
-        } catch (error) {
-            console.log(error.response.data)
-            setErrors(error.response.data.errors)
-        }
-        
+        event.preventDefault();
+        await doRequest();
     }
 
     return(
@@ -33,12 +29,7 @@ export default () => {
                 <label>Password</label>
                 <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control"></input>
             </div>
-            {errors.length > 0 && <div className='alert alert-danger'>
-                <h4>Opss..</h4>
-                <ul>
-                    {errors.map(error => <li key={error.message}>{error.message}</li>)}
-                </ul>
-            </div>}
+            {errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
