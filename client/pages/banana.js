4b0b95a173fdbf8a02b4d8f86a7409e1cd2d7767
JFernando12 <fire.jf2012@gmail.com>
2022-08-31 18:54:38 -0500

    changes

--- client/pages/banana.js
+++ client/pages/banana.js
@@ -1,3 +1,3 @@
 export default () => {
-    return <div>Banana</div>
-}
+  return <div>Banana</div>;
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,15 @@
-import axios from "axios";
-import buildClient from "../api/build-client";
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser);
-    return <h1>Landing page</h1>
-}
+  console.log(currentUser);
+  return currentUser ? <h1>User is log in</h1> : <h1>User not is log in</h1>;
+};
 
 export default LandingPage;
 
-LandingPage.getInitialProps = async(context) => {
-    const client = buildClient(context);
-    const { data } = await client.get('/api/users/currentuser');
-    return data;
-}
+LandingPage.getInitialProps = async (context) => {
+  console.log('Landing Page...');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
