e67ed85caaf5bab853b5b5c46cad570882a12037
JFernando12 <fire.jf2012@gmail.com>
2022-10-01 10:49:39 -0500

    changes

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 if (!process.env.JWT_KEY) {
   throw new Error('jwt_key must exist');
 }
 
-if(!process.env.MONGO_URI) {
+if (!process.env.MONGO_URI) {
   throw new Error('MONGO_URI must exist');
 }
 
@@ -13,6 +14,17 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then((db) => console.log('DB connected:', db.connections[0].name))
   .catch((err) => console.error(err));
+
+natsWrapper
+  .connect('ticketing', 'asdfsdgsdgdf', 'http://nats-srv:4222')
+  .catch((err) => console.error(err));
+
+natsWrapper.client.on('close', () => {
+  console.log('NATS connection closed!');
+  process.exit();
+});
+process.on('SIGINT', () => natsWrapper.client.close());
+process.on('SIGTERM', () => natsWrapper.client.close());
 
 app.listen(3000, () => {
   console.log('Server on port 3000');
