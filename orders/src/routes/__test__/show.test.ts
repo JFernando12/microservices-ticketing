ee4f26d30c57653827f6beb1055450b6368f09b9
JFernando12 <fire.jf2012@gmail.com>
2022-10-20 16:30:34 -0500

    changes

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,52 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('fetches the order', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'ticket prueba',
+    price: 30,
+  });
+  await ticket.save();
+
+  // Create an order
+  const user = global.signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Fetch order
+  const response = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send({});
+
+  expect(response.body.id).toEqual(order.id);
+});
+
+it('returns an error if one user tries to fetch another users order', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'ticket prueba',
+    price: 30,
+  });
+  await ticket.save();
+
+  // Create an order
+  const user = global.signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Fetch order
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send({})
+    .expect(401);
+});
