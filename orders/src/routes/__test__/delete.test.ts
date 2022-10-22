e8ba97b223074ea84a497718cae9edb758f3f78a
Fernando Castrejon <fernandocastrejonh@gmail.com>
2022-10-22 07:19:47 -0500

    changes

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('Mark an order as cancelled', async () => {
   // Create a ticket
@@ -33,4 +34,30 @@ it('Mark an order as cancelled', async () => {
   expect(orderUpdated?.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('Emits an order cancelled event');
+it('Emits an order cancelled event', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'Ticket de prueba',
+    price: 40,
+  });
+  await ticket.save();
+
+  // Create a user cookie
+  const user = global.signin();
+
+  // Create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Cancell order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send({})
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order, OrderStatus } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns an error if the ticket does not exist', async () => {
   const ticketId = new mongoose.Types.ObjectId();
@@ -48,4 +49,20 @@ it('reserves a ticket', async () => {
     .set('Cookie', global.signin())
     .send({ ticketId: ticket.id })
     .expect(201);
+});
+
+it('Emit a ticket created event', async () => {
+  const ticket = Ticket.build({
+    title: 'ticket prueba',
+    price: 30,
+  });
+  ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toBeCalled();
 });
